064447cacdf6b613adc6926fed006e3663d2c19f
Hugo <hugolacerdar@gmail.com>
2021-05-06 19:45:51 -0300

    Test covering async stream build version

--- test/freela_reports_test.exs
+++ test/freela_reports_test.exs
@@ -179,4 +179,193 @@ defmodule FreelaReportsTest do
       assert response == expected
     end
   end
+
+  describe "build_from_many/1" do
+    test "given a list of csv filenames, should return a report" do
+      filenames = ["sample.csv", "sample.csv"]
+
+      response = FreelaReports.build_from_many(filenames)
+
+      expected =
+        {:ok,
+         %{
+           "all_hours" => %{
+             "Cleiton" => 8,
+             "Daniele" => 42,
+             "Danilo" => 12,
+             "Diego" => 14,
+             "Giuliano" => 20,
+             "Jakeliny" => 28,
+             "Joseph" => 6,
+             "Mayk" => 38,
+             "Rafael" => 0,
+             "Vinicius" => 0
+           },
+           "hour_per_year" => %{
+             "Cleiton" => %{"2016" => 6, "2017" => 0, "2018" => 0, "2019" => 0, "2020" => 2},
+             "Daniele" => %{"2016" => 20, "2017" => 6, "2018" => 14, "2019" => 0, "2020" => 2},
+             "Danilo" => %{"2016" => 0, "2017" => 0, "2018" => 0, "2019" => 12, "2020" => 0},
+             "Diego" => %{"2016" => 6, "2017" => 8, "2018" => 0, "2019" => 0, "2020" => 0},
+             "Giuliano" => %{"2016" => 0, "2017" => 6, "2018" => 0, "2019" => 12, "2020" => 2},
+             "Jakeliny" => %{"2016" => 0, "2017" => 16, "2018" => 0, "2019" => 12, "2020" => 0},
+             "Joseph" => %{"2016" => 0, "2017" => 6, "2018" => 0, "2019" => 0, "2020" => 0},
+             "Mayk" => %{"2016" => 14, "2017" => 16, "2018" => 0, "2019" => 8, "2020" => 0},
+             "Rafael" => %{"2016" => 0, "2017" => 0, "2018" => 0, "2019" => 0, "2020" => 0},
+             "Vinicius" => %{"2016" => 0, "2017" => 0, "2018" => 0, "2019" => 0, "2020" => 0}
+           },
+           "hours_per_month" => %{
+             "Cleiton" => %{
+               "abril" => 0,
+               "agosto" => 0,
+               "dezembro" => 0,
+               "fevereiro" => 0,
+               "janeiro" => 0,
+               "julho" => 0,
+               "junho" => 8,
+               "maio" => 0,
+               "março" => 0,
+               "novembro" => 0,
+               "outubro" => 0,
+               "setembro" => 0
+             },
+             "Daniele" => %{
+               "abril" => 14,
+               "agosto" => 0,
+               "dezembro" => 10,
+               "fevereiro" => 0,
+               "janeiro" => 0,
+               "julho" => 0,
+               "junho" => 2,
+               "maio" => 16,
+               "março" => 0,
+               "novembro" => 0,
+               "outubro" => 0,
+               "setembro" => 0
+             },
+             "Danilo" => %{
+               "abril" => 0,
+               "agosto" => 0,
+               "dezembro" => 0,
+               "fevereiro" => 12,
+               "janeiro" => 0,
+               "julho" => 0,
+               "junho" => 0,
+               "maio" => 0,
+               "março" => 0,
+               "novembro" => 0,
+               "outubro" => 0,
+               "setembro" => 0
+             },
+             "Diego" => %{
+               "abril" => 0,
+               "agosto" => 8,
+               "dezembro" => 0,
+               "fevereiro" => 0,
+               "janeiro" => 0,
+               "julho" => 0,
+               "junho" => 0,
+               "maio" => 0,
+               "março" => 0,
+               "novembro" => 0,
+               "outubro" => 0,
+               "setembro" => 6
+             },
+             "Giuliano" => %{
+               "abril" => 2,
+               "agosto" => 0,
+               "dezembro" => 0,
+               "fevereiro" => 18,
+               "janeiro" => 0,
+               "julho" => 0,
+               "junho" => 0,
+               "maio" => 0,
+               "março" => 0,
+               "novembro" => 0,
+               "outubro" => 0,
+               "setembro" => 0
+             },
+             "Jakeliny" => %{
+               "abril" => 0,
+               "agosto" => 0,
+               "dezembro" => 0,
+               "fevereiro" => 0,
+               "janeiro" => 0,
+               "julho" => 16,
+               "junho" => 0,
+               "maio" => 0,
+               "março" => 12,
+               "novembro" => 0,
+               "outubro" => 0,
+               "setembro" => 0
+             },
+             "Joseph" => %{
+               "abril" => 0,
+               "agosto" => 0,
+               "dezembro" => 0,
+               "fevereiro" => 0,
+               "janeiro" => 0,
+               "julho" => 0,
+               "junho" => 0,
+               "maio" => 0,
+               "março" => 6,
+               "novembro" => 0,
+               "outubro" => 0,
+               "setembro" => 0
+             },
+             "Mayk" => %{
+               "abril" => 0,
+               "agosto" => 0,
+               "dezembro" => 10,
+               "fevereiro" => 0,
+               "janeiro" => 0,
+               "julho" => 14,
+               "junho" => 0,
+               "maio" => 0,
+               "março" => 0,
+               "novembro" => 0,
+               "outubro" => 0,
+               "setembro" => 14
+             },
+             "Rafael" => %{
+               "abril" => 0,
+               "agosto" => 0,
+               "dezembro" => 0,
+               "fevereiro" => 0,
+               "janeiro" => 0,
+               "julho" => 0,
+               "junho" => 0,
+               "maio" => 0,
+               "março" => 0,
+               "novembro" => 0,
+               "outubro" => 0,
+               "setembro" => 0
+             },
+             "Vinicius" => %{
+               "abril" => 0,
+               "agosto" => 0,
+               "dezembro" => 0,
+               "fevereiro" => 0,
+               "janeiro" => 0,
+               "julho" => 0,
+               "junho" => 0,
+               "maio" => 0,
+               "março" => 0,
+               "novembro" => 0,
+               "outubro" => 0,
+               "setembro" => 0
+             }
+           }
+         }}
+
+      assert response == expected
+    end
+
+    test "should return an error when a list of filenames is not provided" do
+      response = FreelaReports.build_from_many("banana")
+
+      expected = {:error, "Invalid: a list of strings is required"}
+
+      assert response == expected
+    end
+  end
 end
